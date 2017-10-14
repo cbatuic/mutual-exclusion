e2b790f5903841cfed65e5376368363713ff6259
cbatuic <cbalaman@uic.edu.ph>
2017-10-14 10:58:48 +0800

    serial version

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,12 @@
 def insertion_sort(list):
+	iteration = 0
 	for index in range(1,len(list)):
+		iteration += 1
+		print("iteration", iteration)
 		value = list[index]
 		i = index - 1
 		while i>=0:
+			print(">>>", a)
 			if value < list[i]:
 				list[i+1] = list[i] #shift number in slot i right to slot i+1
 				list[i] = value #shift value left into slot i
@@ -12,4 +16,4 @@ def insertion_sort(list):
 
 a = [7,1,3,5,9,2,3]
 insertion_sort(a)
-print(a)
+print("\nresult:",a)
